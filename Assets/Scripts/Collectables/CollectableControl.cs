a02a473991b4bac061c8e386d12c7b36ac7d3c75
Emeka Agara <66171600+EmekaAgara@users.noreply.github.com>
2023-02-01 19:38:17 +0100

    coins displayed

--- Assets/Scripts/Collectables/CollectableControl.cs
+++ Assets/Scripts/Collectables/CollectableControl.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CollectableControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static int coinCount;
+    public GameObject coinCountDisplay;
 
     // Update is called once per frame
     void Update()
     {
+        coinCountDisplay.GetComponent<Text>().text = "" + coinCount;
         
     }
 }
