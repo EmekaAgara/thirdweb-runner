ab4d35b6dace05c73dc38d56d68ee22b6a2143d4
Emeka Agara <66171600+EmekaAgara@users.noreply.github.com>
2023-02-01 11:29:28 +0100

    generate level done

--- Assets/Scripts/Environment/GenerateLevel.cs
+++ Assets/Scripts/Environment/GenerateLevel.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class GenerateLevel : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public GameObject[] section;
+    public int zPos = 50;
+    public bool creatingSection = false;
+    public int secNum;
     void Update()
     {
-        
+        if (creatingSection == false)
+        {
+            creatingSection = true;
+            StartCoroutine(GenerateSection());
+        }     
+    }
+
+    IEnumerator GenerateSection()
+    {
+        secNum = Random.Range(0, 3);
+        Instantiate(section[secNum], new Vector3(0, 0, zPos), Quaternion.identity);
+        zPos += 50;
+        yield return new WaitForSeconds(2);
+        creatingSection = false;
     }
 }
