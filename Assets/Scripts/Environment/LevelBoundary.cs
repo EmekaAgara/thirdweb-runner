ac37ca11878b112c3437c4bf99ea4da018a4a82f
Emeka Agara <66171600+EmekaAgara@users.noreply.github.com>
2023-02-01 09:57:33 +0100

    player and bridge environment added

--- Assets/Scripts/Environment/LevelBoundary.cs
+++ Assets/Scripts/Environment/LevelBoundary.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class LevelBoundary : MonoBehaviour
 {
-    public static float leftSide = -3.5f;
-    public static float rightSide = 3.5f;
+    public static float leftSide = -6.2f;
+    public static float rightSide = 3.4f;
     public float internalLeft;
     public float internalRight;
     void Update()
