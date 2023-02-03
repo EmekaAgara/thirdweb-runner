07ae2cb772fa7bb57dd209b2e79e3f735e8bfccf
Emeka Agara <66171600+EmekaAgara@users.noreply.github.com>
2023-02-03 06:40:17 +0100

    collider

--- Assets/Scripts/Environment/ObstacleCollision.cs
+++ Assets/Scripts/Environment/ObstacleCollision.cs
@@ -6,11 +6,13 @@ public class ObstacleCollision : MonoBehaviour
 {
     // public AudioSource coinFX;
     public GameObject thePlayer;
+    public GameObject charModel;
 
     void OnTriggerEnter(Collider other)
     {
         this.gameObject.GetComponent<BoxCollider>().enabled = false;
         thePlayer.GetComponent<PlayerMove>().enabled = false;
+        charModel.GetComponent<Animator>().Play("Fall Flat");
     }
 
 }
