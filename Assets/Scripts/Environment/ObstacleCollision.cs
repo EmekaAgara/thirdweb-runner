f617fa29500782705fff66b5f128a7bf03f5f91e
Emeka Agara <66171600+EmekaAgara@users.noreply.github.com>
2023-02-04 10:55:00 +0100

    level control added to all obstacles

--- Assets/Scripts/Environment/ObstacleCollision.cs
+++ Assets/Scripts/Environment/ObstacleCollision.cs
@@ -8,12 +8,14 @@ public class ObstacleCollision : MonoBehaviour
     public GameObject thePlayer;
     public GameObject charModel;
     public AudioSource crashSound;
+    public GameObject levelControl;
 
     void OnTriggerEnter(Collider other)
     {
         this.gameObject.GetComponent<BoxCollider>().enabled = false;
         thePlayer.GetComponent<PlayerMove>().enabled = false;
         charModel.GetComponent<Animator>().Play("Slipping");
+        levelControl.GetComponent<LevelDistance>().enabled = false;
         crashSound.Play();
     }
 
